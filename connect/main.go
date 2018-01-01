58fafeeea1597deda7f98a79725e5eab40a314d6
sabzil <jaehoon@gmail.com>
2018-01-01 17:18:00 +0900

    add connect example

---
+++ connect/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	libvirt "github.com/libvirt/libvirt-go"
+)
+
+func main() {
+	fmt.Println("connect")
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("close")
+	conn.Close()
+
+}
