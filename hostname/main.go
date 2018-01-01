ee3680e42d0ce391cc07c949e1e4ed958cc2956a
sabzil <jaehoon@gmail.com>
2018-01-02 01:50:02 +0900

    hostname example

---
+++ hostname/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	libvirt "github.com/libvirt/libvirt-go"
+)
+
+func main() {
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		panic(err)
+	}
+
+	host_name, _ := conn.GetHostname()
+
+	fmt.Printf("Host Name: %s\n", host_name)
+
+	conn.Close()
+}
